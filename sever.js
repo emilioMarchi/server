f496c2eaf64f1ca4ea098e564c0752e5098d7c68
emilioMarchi <em.98.nmynm@gmail.com>
2022-01-22 22:37:18 -0300

    nuevo commit

--- sever.js
+++ sever.js
@@ -1,11 +1,15 @@
 const express = require('express')
 
+const productsRoute = require('./routes/productsRoute')
+
 const app = express()
 
 app.use('/', (req, res) => {
     res.send('todo ok')
 })
 
-app.listen(process.env.PORT || '3000', () => {
+app.use('products', productsRoute)
+
+app.listen(process.env.PORT || '8080', () => {
     console.log('Todo ok')
 })
